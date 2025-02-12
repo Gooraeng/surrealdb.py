dd06f8843b331e5e397681edb1bfc8e8b37e9e05
Gooraeng <birdyoon1998@gmail.com>
2025-02-13 04:58:56 +0900

    Add response["result"] for Client

--- src/surrealdb/connections/utils_mixin.py
+++ src/surrealdb/connections/utils_mixin.py
@@ -1,11 +1,14 @@
-class UtilsMixin:
+from typing import Any
 
+
+class UtilsMixin:
     @staticmethod
     def check_response_for_error(response: dict, process: str) -> None:
         if response.get("error") is not None:
             raise Exception(f"error {process}: {response.get('error')}")
 
     @staticmethod
-    def check_response_for_result(response: dict, process: str) -> None:
-        if "result" not in response.keys():
+    def check_response_for_result(response: dict, process: str) -> Any:
+        if "result" not in response:
             raise Exception(f"no result {process}: {response}")
+        return response["result"]
